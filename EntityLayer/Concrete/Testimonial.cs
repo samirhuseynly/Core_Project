972f13301070444b1adc83a8e7047487d03440cd
Samir Huseynli <samirhueynli2803@gmail.com>
2022-03-11 16:37:15 +0400

    Added Entities

---
+++ EntityLayer/Concrete/Testimonial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Testimonial
+    {
+        public int TestimonialID { get; set; }
+        public string ClietName { get; set; }
+        public string Company { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
